13949bbd1e0ce23e8529437e093ad9b97f2fff0b
student <anton.zhylitski@gmail.com>
2024-05-14 15:14:37 +0200

    initial commit

---
+++ src/main/java/com/example/WebShop/WebShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.WebShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+}
